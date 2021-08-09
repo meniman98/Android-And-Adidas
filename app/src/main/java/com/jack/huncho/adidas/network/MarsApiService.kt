bfe2d83b3031529a7549a46c8a1b5f17ba3a7a3c
Huncho Jack <meniman98@gmail.com>
2021-08-09 14:24:21 +0200

    created retrofit api service and object

---
+++ app/src/main/java/com/jack/huncho/adidas/network/MarsApiService.kt
@@ -0,0 +1,32 @@
+package com.jack.huncho.adidas.network
+
+import com.jack.huncho.adidas.product.Product
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+
+private const val BASE_URL = "http://10.0.2.2:3001/"
+private const val PRODUCT_ENDPOINT = "product"
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
+
+interface MarsApiService {
+
+    @GET(PRODUCT_ENDPOINT)
+    suspend fun getAllProducts() : List<Product>
+}
+
+object MarsApi {
+    val retrofitService: MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java)
+    }
+}
